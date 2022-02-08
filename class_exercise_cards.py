6179fa260d143f0e917687b4a0c3187e196b894c
pered <47572304+pered@users.noreply.github.com>
2022-02-08 16:55:51 +0100

    Update class_exercise_cards.py

--- class_exercise_cards.py
+++ class_exercise_cards.py
@@ -11,4 +11,17 @@ class Deck():
     cards: Optional[List[Card]] = None
     def __init__(self) -> None:
         self.description = None
+        pass
+    
+class CardStyle():
+    def __init__(self, 
+                 description:str, 
+                 cards:List[{str}], 
+                 suits:List[{str}]) -> None:
+        self.description:str = description
+    
+    def create() -> List[Card]:
+        pass
+        
+    
         
