f20f8f385a30824f518ed90f96c26dfe68b9ace0
pered <47572304+pered@users.noreply.github.com>
2022-02-09 16:47:26 +0100

    Update class_exercise_cards.py

--- class_exercise_cards.py
+++ class_exercise_cards.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
-        
+import itertools 
+       
 class Card:
     def __init__(self,
                  suit: str,
@@ -8,7 +9,7 @@ class Card:
         self.rank = rank
 
 class Deck():
-    cards: Optional[List[Card]] = None
+    cards: Optional[List[Card]] = []
     def __init__(self) -> None:
         self.description = None
         pass
@@ -16,12 +17,28 @@ class Deck():
 class CardStyle():
     def __init__(self, 
                  description:str, 
-                 cards:List[{str}], 
-                 suits:List[{str}]) -> None:
+                 cards:List[str], 
+                 suits:List[str]) -> None:
         self.description:str = description
+        self.cards = cards
+        self.suits = suits
     
-    def create() -> List[Card]:
-        pass
+    def create(self, 
+               deckcards:Deck) -> None:
+        for rank,suit in itertools.product(self.cards,self.suits):
+            deckcards.cards.append(Card(suit,rank))
         
     
-        
+def main():
+    testDeck = Deck()
+    testCardStyle = CardStyle("Test",
+                              ["Ace","King","Queen","Jack","10","9","8","7","6","5","4","3","2","1"],
+                              ["Spades","Hearts","Diamonds","Clubs"])
+    
+    testCardStyle.create(testDeck)
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
