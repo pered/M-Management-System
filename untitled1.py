c91ff61c9c22435f21d8e59d3b550334dc37e49f
pered <47572304+pered@users.noreply.github.com>
2022-03-03 20:36:43 +0100

    Added GoogleCloud Test

---
+++ untitled1.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import os.path
+
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1mCJyUwHad0RBj14d8pvYC1EaCd4mHNwzkDKiUYTcLy4'
+SAMPLE_RANGE_NAME = 'Coffee!A2:E'
+
+creds = None
+creds = service_account.Credentials.from_service_account_file('maximal-copilot-343018-f149332a7912.json',scopes=SCOPES)
+
+def main():
+    service = build('sheets','v4', credentials=creds)
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
