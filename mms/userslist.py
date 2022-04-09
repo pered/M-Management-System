c29cca419011ea72a33c42ba0a537d961260159b
pered <47572304+pered@users.noreply.github.com>
2022-04-09 13:20:40 +0200

    More work on metadata

--- mms/userslist.py
+++ mms/userslist.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import os.path
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
@@ -14,14 +13,13 @@ class UserList:
     
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
     SAMPLE_SPREADSHEET_ID = "10bzoC_M0GOyEB8CH5zY9EXE7tGtxnC8vwJO3tTBDkyA"
-    ADMIN_SAMPLE_RANGE_NAME = 'Admin!A1:E'
-    WHOLESALE_SAMPLE_RANGE_NAME = 'Wholesale!A1:F'
+    ADMIN_SAMPLE_RANGE_NAME = 'Admin!'
+    WHOLESALE_SAMPLE_RANGE_NAME = 'Wholesale!'
     
     creds = service_account.Credentials.from_service_account_file(\
             'maximal-copilot-343018-f149332a7912.json',scopes=SCOPES)
     
-    adminSheet = build('sheets','v4', credentials=creds).spreadsheets()
-    wholesaleSheet = build('sheets','v4', credentials=creds).spreadsheets()    
+    Sheet = build('sheets','v4', credentials=creds).spreadsheets()   
         
     def __init__(self) -> None:
         pass
@@ -30,22 +28,20 @@ class UserList:
     
     @classmethod
     def load(cls) -> None:
-        adminResults = UserList.adminSheet.values().get(spreadsheetId=\
-                            UserList.SAMPLE_SPREADSHEET_ID, range=\
-                                UserList.ADMIN_SAMPLE_RANGE_NAME).execute()
+        results = UserList.Sheet.values().batchGet(\
+                            spreadsheetId = UserList.SAMPLE_SPREADSHEET_ID,\
+                            ranges=[UserList.ADMIN_SAMPLE_RANGE_NAME, UserList.WHOLESALE_SAMPLE_RANGE_NAME]).execute()
+           
             
-        wholesaleResults = UserList.wholesaleSheet.values().get(spreadsheetId=\
-                            UserList.SAMPLE_SPREADSHEET_ID, range=\
-                                UserList.WHOLESALE_SAMPLE_RANGE_NAME).execute() 
             
         #Creating DataFrames for creation of user objects
     
-        admin_df = DataFrame(adminResults['values'][1:], columns = adminResults['values'][0])
-        wholesale_df = DataFrame(wholesaleResults['values'][1:], columns = wholesaleResults['values'][0])
+        # admin_df = DataFrame(adminResults['values'][1:], columns = adminResults['values'][0])
+        # wholesale_df = DataFrame(wholesaleResults['values'][1:], columns = wholesaleResults['values'][0])
         
-        ##Check this ##
-        cls.userList += [Admin(x[1]) for x in admin_df.iterrows()]
-        cls.userList += [WholesaleUser(x[1]) for x in wholesale_df.iterrows()]
+        # ##Check this ##
+        # cls.userList += [Admin(x[1]) for x in admin_df.iterrows()]
+        # cls.userList += [WholesaleUser(x[1]) for x in wholesale_df.iterrows()]
     
     @classmethod
     def reload(cls) -> None:
--- untitled1.py
+++ untitled1.py
@@ -13,7 +13,8 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 # SAMPLE_RANGE_NAME = 'Coffee!A2:E'
 
 SAMPLE_SPREADSHEET_ID = "10bzoC_M0GOyEB8CH5zY9EXE7tGtxnC8vwJO3tTBDkyA"
-SAMPLE_RANGE_NAME = 'Admin!A1:E'
+ADMIN_SAMPLE_RANGE_NAME = 'Admin'
+WHOLESALE_SAMPLE_RANGE_NAME = 'Wholesale'
 
 creds = None
 creds = service_account.Credentials.from_service_account_file('maximal-copilot-343018-f149332a7912.json',scopes=SCOPES)
@@ -21,20 +22,32 @@ creds = service_account.Credentials.from_service_account_file('maximal-copilot-3
 
 service = build('sheets','v4', credentials=creds)
 sheet = service.spreadsheets()
-# result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+result = sheet.values().batchGet(spreadsheetId=SAMPLE_SPREADSHEET_ID, ranges=[ADMIN_SAMPLE_RANGE_NAME,WHOLESALE_SAMPLE_RANGE_NAME])
+response = result.execute()
 
+print([i['values'] for i in response['valueRanges']])
 
 
 # create_developer_metadata = {"requests" :[{"createDeveloperMetadata": {"developerMetadata": {
-#                                                                 "metadataKey":"Admin1",
-#                                                                 "location" : {"dimensionRange":
-#                                                                               {"sheetId":1929008158,
-#                                                                               "dimension":"ROWS",
-#                                                                               "startIndex": 1,
-#                                                                               "endIndex": 2}
-#                                                                               },
-#                                                                 "visibility": "DOCUMENT"}
-#     }}]}
+#                                                                  "metadataKey":"Pere",
+#                                                                  "location" : {"dimensionRange":
+#                                                                                {"sheetId":1929008158,
+#                                                                                "dimension":"ROWS",
+#                                                                                "startIndex": 1,
+#                                                                                "endIndex": 2}
+#                                                                                },
+#                                                                  "visibility": "DOCUMENT"}
+#      }},
+#                                             {"createDeveloperMetadata": {"developerMetadata": {
+#                                                                  "metadataKey":"Test",
+#                                                                  "location" : {"dimensionRange":
+#                                                                                {"sheetId":1929008158,
+#                                                                                "dimension":"ROWS",
+#                                                                                "startIndex": 2,
+#                                                                                "endIndex": 3}
+#                                                                                },
+#                                                                  "visibility": "DOCUMENT"}
+#      }}]}
 
 # request = service.spreadsheets().batchUpdate(spreadsheetId=SAMPLE_SPREADSHEET_ID, body=create_developer_metadata)
 # response = request.execute()
@@ -57,8 +70,21 @@ sheet = service.spreadsheets()
 
 
 # #This deletes all previously applied metadata on a sheet
-delete_all = {"requests" :{"deleteDeveloperMetadata":{"dataFilter": {"developerMetadataLookup": {"metadataLocation": {"sheetId":1929008158}}}}}}
-request = service.spreadsheets().batchUpdate(spreadsheetId=SAMPLE_SPREADSHEET_ID, body=delete_all)
-response = request.execute()
+# delete_all = {"requests" :{"deleteDeveloperMetadata":{"dataFilter": {"developerMetadataLookup": {"metadataLocation": {"sheetId":1929008158}}}}}}
+# request = service.spreadsheets().batchUpdate(spreadsheetId=SAMPLE_SPREADSHEET_ID, body=delete_all)
+# response = request.execute()
 
-print(response)
+#Updating values by the use of metadata ID
+
+# batch_datafilter_update = {"valueInputOption": 'USER_ENTERED',
+#                            "data": [{"dataFilter": {'developerMetadataLookup': 
+#                                                    {'metadataId': 265876189
+#                                                     }},
+#                             "majorDimension": 'ROWS',
+#                             "values": [[None,'Ok',None,None,None]
+#                                        ]}]}
+
+# request = sheet.values().batchUpdateByDataFilter(spreadsheetId=SAMPLE_SPREADSHEET_ID, body=batch_datafilter_update)
+# response = request.execute()
+
+#print(response)
